8340e69a0f89c2a5304a5fa11846ae86173fc5d1
Abdel Cartagena <abdel_123@hotmail.es>
2025-10-12 07:15:07 -0500

    Actualizar end-poinst

--- app/api/apiClient.ts
+++ app/api/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-    baseURL: 'http://localhost:8000/api', // 🔥 Verifica que esta URL sea correcta
+    baseURL: 'https://app.portafolioerp.com/api', // 🔥 Verifica que esta URL sea correcta
     headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json',
